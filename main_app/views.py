56271f9a7d11c6b2fcbcb34d7944268ca39f43fd
Thomas Strub <Thom@Thomass-Air.wavecable.com>
2020-12-15 15:31:40 -0800

    setup urls and basic home and about routes

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello</h1>')
+
+def about(request):
+    return HttpResponse('<h1>About</h1>')
