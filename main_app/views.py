0b0bbaa4b1ca81c71923669b8d859b2f2d974cf8
Thomas Strub <Thom@Thomass-Air.wavecable.com>
2020-12-17 20:53:41 -0800

    RecommendationCityList routes to recommendation form for cities in CITIES constant

--- main_app/views.py
+++ main_app/views.py
@@ -110,10 +110,6 @@ class RecommendationCityList(ListView):
   template_name = 'recommendations/recommendation_city.html'
   def get_queryset(self):
     print(self.kwargs['city'], "self.kwargs['city']<------------------")
-    # if self.kwargs['city'] == 'seattle':
-    #   print("seattle in kwargs <--------------")
-    #   self.kwargs['city'] = 'S'
-    # elif self.kwargs['city'] == 'portland':
     for c in CITIES:
       if self.kwargs['city'] == c[1].lower().replace(" ", ""):
         print(self.kwargs['city'], "city kwarg <---------===")
