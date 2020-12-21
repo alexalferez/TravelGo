e02b0268b77a7cb916a081ed7cc33386000aa021
Alex Alferez <alexalferez@alexs-mbp.home>
2020-12-21 13:36:38 -0800

    working comments

--- main_app/views.py
+++ main_app/views.py
@@ -3,7 +3,7 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile, Recommendation, Photo, CITIES
+from .models import Profile, Recommendation, Photo, CITIES, Comment
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import RecommendationForm, CommentForm
@@ -61,9 +61,9 @@ def about(request):
 
 def recommendations_detail(request, recommendation_id):
   recommendation = Recommendation.objects.get(id=recommendation_id)
-  comment = Comment.objects.get(id=recommendation_id)
+
   print(recommendation.__dict__, "name of town")
-  return render(request, './recommendations/detail.html', { 'recommendation': recommendation}, {'comment':comment})
+  return render(request, './recommendations/detail.html', { 'recommendation': recommendation})
 
   
 
@@ -91,10 +91,12 @@ class ProfileDelete(DeleteView):
   model = Profile
   success_url = ''
 
-def add_comment(request, recommendation_id):
+def add_comment(request,recommendation_id):
   form = CommentForm(request.POST)
+  user = Recommendation.objects.get(id=recommendation_id).user
   if form.is_valid():
     new_comment = form.save(commit=False)
+    new_comment.user = user
     new_comment.recommendation_id = recommendation_id
     new_comment.save()
   return redirect('detail', recommendation_id=recommendation_id)
