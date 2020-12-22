d1796686e01944a69251d4dab891de8a090bfda6
Thomas Strub <Thom@Thomass-Air.wavecable.com>
2020-12-22 11:26:32 -0800

    adjusted .env and settings.py

--- TravelGo/settings.py
+++ TravelGo/settings.py
@@ -13,11 +13,10 @@ import os
 import environ
 environ.Env()
 environ.Env.read_env()
-from environs import Env
+
 from pathlib import Path
 
-env = Env()
-env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -34,8 +33,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-GOOGLE_MAPS_API_KEY = env.str('GOOGLE_MAPS_API_KEY')
-GOOGLE_MAPS_SECRET = env.str('GOOGLE_MAPS_SECRET')
+
 
 # Application definition
 
